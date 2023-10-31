59d217d71180543f5e032fd41fbf0681ebc12ea3
Cristina Amoedo <gizanakris@gmail.com>
2023-10-31 08:46:37 +0100

    Project creation and introduction of libraries

---
+++ MysticLotus/MysticLotus/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MysticLotus
+//
+//  Created by Cristina Amoedo Fragueiro on 31/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
